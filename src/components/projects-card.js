a7492f52aff5a1a319b68d717de6bad40bfa248b
AlanPL <apyolt@gmail.com>
2021-10-01 00:36:19 -0500

    update projects data

--- src/components/projects-card.js
+++ src/components/projects-card.js
@@ -1,4 +1,4 @@
-import githubIcon from '../images/github@2x.png';
+import { Icon } from '@iconify/react';
 
 function ProjectCard(props) {
   const angleDif = 30;//180/props.technologies.length;
@@ -19,6 +19,30 @@ function ProjectCard(props) {
     )
   });
 
+  const renderLinks = ()=>{
+    if (props.page==="") {
+      return (
+        <a href={props.repo} target="_blank" rel="noreferrer">
+          <Icon icon="carbon:logo-github" style={{ fontSize: '64px' }} className="absolute inset-x-0 top-16 m-auto w-12
+              md:top-20 md:w-14
+              hover:-translate-y-1 transition duration-200 transform" />
+        </a>)
+    } else {
+       return (
+         <div className="absolute inset-x-0 top-20 m-auto md:top-24">
+            <a href={props.repo} target="_blank" rel="noreferrer" className="m-auto inline-block mr-3">
+              <Icon icon="carbon:logo-github" style={{ fontSize: '50px' }} className="w-10 md:w-14
+                  hover:-translate-y-1 transition duration-200 transform" />
+            </a>
+            <a href={props.page} target="_blank" rel="noreferrer" className="m-auto inline-block ml-3">
+              <Icon icon="carbon:launch" style={{ fontSize: '50px' }} className="w-10 md:w-14
+                  hover:-translate-y-1 transition duration-200 transform" />
+            </a>
+         </div>
+        )     
+    }
+  }
+
   return (
     <div className="block text-gray-light m-auto group font-light">
       <span className="text-2xl block ">{props.name}</span> 
@@ -31,11 +55,7 @@ function ProjectCard(props) {
               <div className="h-full  bg-blue-dark bg-opacity-70 w-full relative flex 
                     media-hover:hidden
 		                group-hover:flex ">
-                    <a href={props.repo} target="_blank" rel="noreferrer">
-                      <img alt="github repo" src={githubIcon} className="absolute inset-x-0 top-16 m-auto w-12
-                      md:top-20 md:w-14
-                      hover:-translate-y-1 transition duration-200 transform"/>
-                    </a>
+                    {renderLinks()}
                     {renderTechnologies}
                     <div className="flex mt-28  py-5 px-12 h-32 max-h-32
                                     md:mt-32 md:h-36 md:max-h-36
